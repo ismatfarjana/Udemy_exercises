0cce2c8a7e8bb7168de8971b1f1da19162e27b43
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2019-07-07 09:51:51 +1000

    add fizzbuzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+
+def fizzbuzz(number)
+ i = 1
+ while i <= number
+  if i % 3 == 0 && i % 5 == 0
+    puts "fizzbuzz"
+  elsif i % 3 == 0
+      puts "fizz"
+  elsif i % 5 == 0
+      puts "buzz"
+  end
+  i +=1
+ end
+end
+fizzbuzz 75
