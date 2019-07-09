5ef8265d67e8e8eaf883768eda13235aaba74e8a
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2019-07-09 11:35:01 +1000

    learning push shovel, insert in array

---
+++ push_shovel_insert.rb
@@ -0,0 +1,14 @@
+#push
+family = ["tanim","dipty"]
+family.push "isabela"
+p family
+
+#shovel
+ 
+family << "isabela"
+p family
+
+#insert
+family = ["tanim","dipty"]
+family.insert(1,"isabela")
+p family
